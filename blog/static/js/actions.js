60dc7709bd353a389c002457f587f1513df07e9e
Artem.H <art.pzt@gmail.com>
2018-07-15 16:52:52 +0300

    prev/next button without reload page)

--- blog/static/js/actions.js
+++ blog/static/js/actions.js
@@ -3,44 +3,55 @@ function get_hostname(url) {
     return m ? m[0] : null;
 };
 
-
 var currentUrl = get_hostname(document.URL) + '/blog/all/' ;
-//
-// $("#content").clone().appendTo("#blog-content");
-// $("#content").clone().appendTo("#blog-content");onClick="window.location.href='http://ya.ru'"
 
-function getData (currentUrl) {
-    $.getJSON(currentUrl, function (data) {
-        // alert(currentUrl);
+function getData (Url) {
+    alert('in get data')
+    $.getJSON(Url, function (data) {
+
+        $('#blog-content').empty();
+
         for (var i in data.results) {
             var newArticle = $("#content").clone();
+            //filling HTML from data
             newArticle.attr("style", "");
             newArticle.find("#article-image").attr("src", data.results[i].image.slice(currentUrl.length-1));
+            newArticle.find("#article-image").attr("alt", 'data.results[i].header');
             newArticle.find("#header-text").text(data.results[i].header);
             newArticle.find("#article-short").text(data.results[i].text);
             newArticle.find("#publishied-date").text(data.results[i].publish_date);
-            // alert(get_hostname(document.URL) + '/blog/id/' + data.results[i].id)
-            newArticle.find("#article-button").attr("href", get_hostname(document.URL) + '/blog/id/' + data.results[i].id +'/');
+            newArticle.find("#article-button").attr("href", get_hostname(document.URL) + '/blog/id/' + data.results[i].id );
             newArticle.find("#article-button").attr("target","_blank");
             $('#blog-content').append(newArticle);
         };
-        nexPage = data.next;
-        prevPage = data.previous;
+
+        if ((data.next != null) || (data.previous != null)){
+            $(".nav-section").attr("style", "");
+        }
+
+        $("#article-button-prev").click(function(){
+            if (data.previous != null) {
+                alert("prev!");
+                getData (data.previous)
+            }
+        });
+
+        $("#article-button-next").click(function(){
+            if (data.next != null) {
+                alert("next!");
+                getData (data.next)
+            }
+        });
     });
 };
 
-function buttonNext() {
-    if (window.nexPage != null) {
-        var address = window.nexPage
-        $('#data-container').empty();
-        getData(address);
-    }
-};
 
-function buttonPrev() {
-    if (window.prevPage != null) {
-        var address = window.prevPage
-        $('#data-container').empty();
-        getData(address);
-    }
-};
+// function buttonNext(window.dataNext) {
+//     $('#blog-content').empty();
+//     getData(dataNext);
+// };
+//
+// function buttonPrev(dataPrevious) {
+//     $('#blog-content').empty();
+//     getData(dataPrevious);
+// };
